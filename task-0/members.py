252d91c3fbbf3f72f133cc5c22bcef063f5daff0
Pasitpoon <144659219+Pasitpoon@users.noreply.github.com>
2023-09-11 15:17:37 +0700

    Update members.py

--- task-0/members.py
+++ task-0/members.py
@@ -1,4 +1,4 @@
 [
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
-    {"name": "Cholwich Nattee", "sid": "6422771111"},
+    {"name": "Thanapoom Thanyasukpaisal", "sid": "6422780674"},
+    {"name": "Pasit Poonsiripat", "sid": "6422780195"},
 ]
